38f926c06912727ffe73401adb9896c902e4cb44
Bassem Mohamed <bassemmohamed1994@gmail.com>
2023-02-08 12:01:06 +0100

    Add setQuery logic

--- src/components/repoFinder/RepoFinder.tsx
+++ src/components/repoFinder/RepoFinder.tsx
@@ -1,18 +1,68 @@
-import React from "react";
+import { ApolloError, useQuery } from "@apollo/client";
+import React, { useState } from "react";
 import styled from "styled-components";
-import Search from "../search/Search";
+import SEARCH_REPOS_QUERY from "../../queries/searchRepo";
+import Header from "../header/Header";
 
 const StyledRepoFinder = styled.div`
-  > article {
-    padding: 0 0.5rem;
-    margin: 0.5rem 0;
+  > div {
+    padding: 1rem 1.5rem;
   }
 `;
 
+const Loading = () => (
+  <p>
+    Loading{" "}
+    <span role="img" aria-label="emoji">
+      ⏱⏱
+    </span>
+    ...
+  </p>
+);
+
+const Error = ({ error }: { error: ApolloError }) => (
+  <p>
+    Error{" "}
+    <span role="img" aria-label="emoji">
+      😞
+    </span>
+    {error.toString()}
+  </p>
+);
+
 export default function RepoFinder() {
+  const [query, setQuery] = useState<string>("");
+  const { loading, error, data } = useQuery(SEARCH_REPOS_QUERY, {
+    variables: { query },
+  });
+
+  const reposLoaded = query && data && !loading;
+  const repos = data?.search.edges;
+
   return (
     <StyledRepoFinder>
-      <Search onSubmit={(query) => {}}></Search>
+      {/* Header */}
+      <Header onSubmit={(query) => setQuery(query)}></Header>
+
+      {/* Main */}
+      <div>
+        {loading && <Loading />}
+        {error && <Error error={error} />}
+
+        {/* Search output */}
+        {reposLoaded && repos.map((item: any) => item.node.name)}
+
+        {/* No search query */}
+        {!reposLoaded && (
+          <p>
+            Start by searching for a github repository. For example, Try
+            "Arabic-English-Subtitling"{" "}
+            <span role="img" aria-label="emoji">
+              😊
+            </span>
+          </p>
+        )}
+      </div>
     </StyledRepoFinder>
   );
 }
